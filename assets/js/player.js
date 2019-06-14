4b548eb04e35917d71af0ac73e8a13cbded4325f
glee1228 <glee1228@users.noreply.github.com>
2019-06-14 23:54:51 +0900

    update 2차 완성본

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,53 +1,52 @@
+
+
 var videoplayer_step = function(i) {
-    // $('#description').empty();
+
     i-=1;
-    // console.log($('#player'));
-    $(".player-container").empty();
     console.log(i);
-
-
-
+    var myframe = document.getElementById("player");
+    if(i==0){
+        myframe.setAttribute("width", "0");
+    }
     if(i==1) {
-        $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="eKodxomtlNk"></div>');
+        player.playVideoAt(0);
+        myframe.setAttribute("width", "640");
 
     }
     else if(i==2){
         // $(".player-container").append('<video width="100%" height="auto" id="autoplay" controls crossorigin playsinline poster="assets/img/2_malmoi.png">><source src="assets/video/2_malmoi.mp4" type="video/mp4"></video>');
-        $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="-uSAbEix8i4"></div>');
+        player.playVideoAt(1);
     }
     else if(i==3){
         // $(".player-container").append('<video width="100%" height="auto" id="autoplay" controls crossorigin playsinline poster="assets/img/3_gwihyang.png"><source src="assets/video/3_gwihyang.mp4" type="video/mp4"></video>');
-    $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="uHTuwVaLglo"></div>');
+       player.playVideoAt(2);
     }
     else if(i==4) {
         // $(".player-container").append('<video width="100%" height="auto" id="autoplay" controls crossorigin playsinline poster="assets/img/4_gunhamdo.png"><source src="assets/video/4_gunhamdo.mp4" type="video/mp4"></video>');
-    $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="VI5gx0SqYNo"></div>');}
-    else if(i==5){
+        player.playVideoAt(3);
+    }
+    else if(i==5) {
         // $(".player-container").append('<video width="100%" height="auto" id="autoplay" controls crossorigin playsinline poster="assets/img/5_taegeuggi.png"><source src="assets/video/5_taegeuggi.mp4" type="video/mp4"></video>');
-    $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="pQVTQICYRmE"></div>');}
-    else if(i==6){
+        player.playVideoAt(4);
+    }else if(i==6) {
         // $(".player-container").append('<video width="100%" height="auto" id="autoplay" controls crossorigin playsinline poster="assets/img/6_hyojadong.png"><source src="assets/video/6_hyojadong.mp4" type="video/mp4"></video>');
-    $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="94Q3IPBXF7k"></div>');}
+        player.playVideoAt(5);
+    }
     else if(i==7) {
         // $(".player-container").append('<video width="100%" height="auto" id="autoplay" controls crossorigin playsinline poster="assets/img/7_then_those_people.png"><source src="assets/video/7_then_those_people.mp4" type="video/mp4"></video>');
-    $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="0Vo6vEAYyRY"></div>');}
-    else if(i==8){
+        player.playVideoAt(6);
+    }else if(i==8) {
         // $(".player-container").append('<video width="100%" height="auto" id="autoplay" controls crossorigin playsinline poster="assets/img/8_jeontaeil.png"><source src="assets/video/8_jeontaeil.mp4" type="video/mp4"></video>');
-    $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="RfpzGlBaoBo"></div>');}
-    else if(i==9){
+        player.playVideoAt(7);
+    }else if(i==9) {
         // $(".player-container").append('<video width="100%" height="auto" id="autoplay" controls crossorigin playsinline poster="assets/img/9_colorful_holiday.png"><source src="assets/video/9_colorful_holiday.mp4" type="video/mp4"></video>');
-    $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="2mJy32X43Og"></div>'); }
-    else if(i==10){
+        player.playVideoAt(8);
+    }else if(i==10){
         // $(".player-container").append('<video width="100%" height="auto" id="autoplay" controls crossorigin playsinline poster="assets/img/10_taxi_driver.png"><source src="assets/video/10_taxi_driver.mp4" type="video/mp4"></video>');
-    $(".player-container").append('<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="-eExpGoHEXg"></div>');
+        player.playVideoAt(9);
     }
-    else{
 
-    }
-    const player = new Plyr('#player', {});
 
-// Expose player so it can be used from the console
-window.player = player;
-    // const player = new Plyr('#autoplay');
-    // player.play();
+
+
 };
